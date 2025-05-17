e7a81b8d1fe64b65a7b9979d8feea505ce61f863
wwsjsjjs <896528971@qq.com>
2025-05-17 19:26:39 +0800

    配置前后端

--- tutorial_platform/src/main/java/org/tutorial/tutorial_platform/config/WebMvcConfig.java
+++ tutorial_platform/src/main/java/org/tutorial/tutorial_platform/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package org.tutorial.tutorial_platform.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.tutorial.tutorial_platform.util.JwtInterceptor;
@@ -32,5 +33,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "/api/auth/login",   // 登录接口
                         "/api/auth/register" // 注册接口
                 );
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 对所有路径生效
+                .allowedOrigins("http://localhost:8080") // 允许的前端域名
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedHeaders("*") // 允许的请求头
+                .allowCredentials(true); // 是否允许发送 Cookie
     }
 } 
