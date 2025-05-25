388212ccfac16e6a1eb52e80b1a2b4c24da3fce7
郑晓丰 <15552869+SYSU-Feng@user.noreply.gitee.com>
2025-05-25 12:18:58 +0800

    聊天数据库

--- tutorial_platform/src/main/java/org/tutorial/tutorial_platform/dto/PageDTO.java
+++ tutorial_platform/src/main/java/org/tutorial/tutorial_platform/dto/PageDTO.java
@@ -6,8 +6,8 @@ import lombok.Data;
 
 @Data
 public class PageDTO {
-    @Min(value = 1, message = "页码必须≥1")
-    private int page = 1;
+    @Min(value = 0, message = "页码必须≥0")
+    private int page = 0;
 
     @Min(value = 1, message = "每页条数最小为1")
     @Max(value = 100, message = "每页条数最大为100")
